155d2ce27e1fef1659a7a950029a11934ea5481b
GaoHongwei <gaohongwei931011@sina.com>
2019-07-01 23:01:52 +0800

    图灵api/v2

--- test.py
+++ test.py
@@ -1,39 +1,41 @@
-#coding=utf8
-import requests
+
+# -*- coding: utf-8 -*-
+ 
 import itchat
-
-KEY = '8edce3ce905a4c1dbb965e6b35c3834d'
-
-def get_response(msg):
-    # 这里我们就像在“3. 实现最简单的与图灵机器人的交互”中做的一样
-    # 构造了要发送给服务器的数据
-    apiUrl = 'http://www.tuling123.com/openapi/api'
-    data = {
-        'key'    : KEY,
-        'info'   : msg,
-        'userid' : 'wechat-robot',
-    }
-    try:
-        r = requests.post(apiUrl, data=data).json()
-        # 字典的get方法在字典没有'text'值的时候会返回None而不会抛出异常
-        return r.get('text')
-    # 为了防止服务器没有正常响应导致程序异常退出，这里用try-except捕获了异常
-    # 如果服务器没能正常交互（返回非json或无法连接），那么就会进入下面的return
-    except:
-        # 将会返回一个None
-        return
-
-# 这里是我们在“1. 实现微信消息的获取”中已经用到过的同样的注册方法
-@itchat.msg_register(itchat.content.TEXT)
-def tuling_reply(msg):
-    # 为了保证在图灵Key出现问题的时候仍旧可以回复，这里设置一个默认回复
-    defaultReply = 'I received: ' + msg['Text']
-    # 如果图灵Key出现问题，那么reply将会是None
-    reply = get_response(msg['Text'])
-    # a or b的意思是，如果a有内容，那么返回a，否则返回b
-    # 有内容一般就是指非空或者非None，你可以用`if a: print('True')`来测试
-    return reply or defaultReply
-
-# 为了让实验过程更加方便（修改程序不用多次扫码），我们使用热启动
+import requests
+import json
 itchat.auto_login(hotReload=True)
+ 
+friends = itchat.get_friends()
+ 
+def getResponse(msg):
+    url = "http://openapi.tuling123.com/openapi/api/v2"
+    data = {
+    	"reqType":0,
+        "perception": {
+            "inputText": {
+                "text": msg
+            },
+            "inputImage": {
+                "url": "imageUrl"
+            }
+        },
+        "userInfo": {
+            "apiKey": "81a0447221504656bff04b47d1a8c868",
+            "userId": "Geclipse"
+        }
+    }
+    data = json.dumps(data)
+    r = requests.post(url,data).json()
+    return r['results'][0]['values']['text']
+ 
+ 
+@itchat.msg_register(itchat.content.TEXT)    #读取接受到的消息的TEXT部分内容,存储进默认的msg
+ 
+def auto_reply(msg):
+    friends_message = msg['Text']
+    name = msg['FromUserName']
+    reply = getResponse(friends_message)
+    itchat.send(reply,toUserName=name)
+ 
 itchat.run()
